bb873af2eaa0ed8a662182cd806a736a08078b3b
tinaserra <vserra@student.42.fr>
2022-10-31 16:49:53 +0100

    chat page 90% -> merge

--- back/dist/auth/dto/basicLogin.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BasicLoginDto = void 0;
-class BasicLoginDto {
-}
-exports.BasicLoginDto = BasicLoginDto;
-//# sourceMappingURL=basicLogin.dto.js.map
