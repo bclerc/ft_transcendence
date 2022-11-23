0d6dd6fee2a0f6004ca4a2df8138e4ab0bfb2bd6
bclerc <bclerc@student.42.fr>
2022-11-23 19:29:23 +0100

    Rank

--- back/src/game/game.service.ts
+++ back/src/game/game.service.ts
@@ -13,7 +13,7 @@ export class GameService implements OnModuleInit {
     private prisma: PrismaService,
     private userService: UserService
   ) { }
-  
+
   async onModuleInit() {
     await this.prisma.game.deleteMany({
       where: {
@@ -32,7 +32,6 @@ export class GameService implements OnModuleInit {
         }
       }
     });
-    console.log("yo frero");
   }
 
 
