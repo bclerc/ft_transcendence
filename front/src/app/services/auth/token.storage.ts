45fda778be0ebc741081205cc2a8fb9af28820b1
NapinGit <aloiseau@DESKTOP-T13MJOT.localdomain>
2022-11-23 19:33:51 +0100

    push

--- front/src/app/services/auth/token.storage.ts
+++ front/src/app/services/auth/token.storage.ts
@@ -8,21 +8,21 @@ const USER_KEY = 'auth-user';
 export class TokenStorageService {
   constructor(private jwtHelper: JwtHelperService) { }
   signOut(): void {
-    window.sessionStorage.clear();
+    window.localStorage.clear();
   }
   public saveToken(token: string): void {
-    window.sessionStorage.removeItem(TOKEN_KEY);
-    window.sessionStorage.setItem(TOKEN_KEY, token);
+    window.localStorage.removeItem(TOKEN_KEY);
+    window.localStorage.setItem(TOKEN_KEY, token);
   }
   public getToken(): string | null {
-    return window.sessionStorage.getItem(TOKEN_KEY);
+    return window.localStorage.getItem(TOKEN_KEY);
   }
   public saveUser(user: any): void {
-    window.sessionStorage.removeItem(USER_KEY);
-    window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
+    window.localStorage.removeItem(USER_KEY);
+    window.localStorage.setItem(USER_KEY, JSON.stringify(user));
   }
   public getUser(): any {
-    const user = window.sessionStorage.getItem(USER_KEY);
+    const user = window.localStorage.getItem(USER_KEY);
     if (user) {
       return JSON.parse(user);
     }
@@ -42,6 +42,6 @@ export class TokenStorageService {
   }
   public removeToken(): void
   {
-    window.sessionStorage.removeItem(TOKEN_KEY);
+    window.localStorage.removeItem(TOKEN_KEY);
   }
 }
